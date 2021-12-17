b75c42897d5ba2ac7883ae6c9a429afa7ae51fd9
johankasperi <johan@kasperi.se>
2021-12-17 14:55:53 +0100

    Upgrade example src to use resize worklet

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,55 @@
-import * as React from 'react';
-
-import { StyleSheet, View, Text } from 'react-native';
-import VisionCameraResizePlugin from 'vision-camera-resize-plugin';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, ActivityIndicator } from 'react-native';
+import { useSharedValue } from 'react-native-reanimated';
+import {
+  Camera,
+  useCameraDevices,
+  useFrameProcessor,
+} from 'react-native-vision-camera';
+import { resize } from 'vision-camera-resize-plugin';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const [hasPermission, setHasPermission] = useState(false);
+  const currentLabel = useSharedValue('');
 
-  React.useEffect(() => {
-    VisionCameraResizePlugin.multiply(3, 7).then(setResult);
+  const devices = useCameraDevices();
+  const device = devices.back;
+
+  useEffect(() => {
+    (async () => {
+      const status = await Camera.requestCameraPermission();
+      setHasPermission(status === 'authorized');
+    })();
   }, []);
+
+  const frameProcessor = useFrameProcessor(
+    (frame) => {
+      'worklet';
+      resize(
+        frame,
+        0,
+        0,
+        frame.width / 50,
+        frame.width / 50,
+        frame.width / 50,
+        frame.width / 50
+      );
+    },
+    [currentLabel]
+  );
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      {device != null && hasPermission ? (
+        <Camera
+          style={styles.camera}
+          device={device}
+          isActive={true}
+          frameProcessor={frameProcessor}
+        />
+      ) : (
+        <ActivityIndicator size="large" color="white" />
+      )}
     </View>
   );
 }
@@ -22,10 +59,10 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'black',
   },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
+  camera: {
+    flex: 1,
+    width: '100%',
   },
 });
